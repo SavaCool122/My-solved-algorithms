ee9a26cb5a924b16c004971263ee083e1fe616bd
SavaCool122 <mrsavacool@gmail.com>
2020-05-19 16:28:01 +0300

    change best solution

--- findUniqSecond/best-fast-sol.js
+++ findUniqSecond/best-fast-sol.js
@@ -1,21 +1,18 @@
 let t = ['Aa', 'aaa', 'aaaaa', 'BbBb', 'Aaaa', 'AaAaAa', 'a']
 let c = ['abc', 'acb', 'bac', 'foo', 'bca', 'cab', 'cba']
 let g = ['Tom Marvolo Riddle', 'I am Lord Voldemort', 'Harry Potter']
+
 function findUniq(arr) {
-  let [a, b, c] = arr.slice(0, 3)
-
-  if (!similar(a, b) && !similar(a, c)) return a
-  for (d of arr) if (!similar(a, d)) return d
+  var binA = wordOccurence(arr[0]),
+    binB = wordOccurence(arr[1])
+  return arrayEqual(binA, binB)
+    ? arr.find((w) => !arrayEqual(binA, wordOccurence(w)))
+    : [binA, binB].find((b) => !arrayEqual(b, wordOccurence(arr[2])))
 }
 
-function similar(x, y) {
-  x = new Set(x.toLowerCase())
-  y = new Set(y.toLowerCase())
-
-  if (x.size !== y.size) return false
-  for (z of x) if (!y.has(z)) return false
-
-  return true
-}
-
-console.log(findUniq(g))
+var wordOccurence = (str) =>
+  Object.keys(
+    [...str.toLowerCase()].reduce((b, c) => ((b[c] = true), b), {}),
+  ).sort()
+var arrayEqual = (a, b) =>
+  a.length === b.length && a.every((e, i) => e === b[i])
